0f5c52fbd17c0df3bbeb66b6139d244432b48fb8
freakphp <me@freakphp.com>
2011-12-05 16:30:53 +0100

    Fix typo

--- Form/Type/UserDiscountFormType.php
+++ Form/Type/UserDiscountFormType.php
@@ -5,18 +5,18 @@ namespace Hatimeria\BankBundle\Form\Type;
 use Symfony\Component\Form\AbstractType,
     Symfony\Component\Form\FormBuilder;
 
-class UserFormType extends AbstractType
+class UserDiscountFormType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder->add("discount");
     }
 
-    public function getParent(array $options)
-    {
-        return 'form';
-    }
-
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
     function getName()
     {
         return 'user_discount_form';
